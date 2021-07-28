d18d14a436c858671997d21cf93c66e5a369925d
GuilhermeAAmaral <gui_lhermeaugusto@live.com>
2021-07-27 21:27:28 -0300

    atualização

--- semana7/labenusers/src/App.js
+++ semana7/labenusers/src/App.js
@@ -3,13 +3,15 @@ import styled from 'styled-components'
 import axios from 'axios';
 
 
+
 const ContainerPrincipal = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
+font-family: Open Sans;
 text-align: center;
-margin-top: 250px;
 padding: 20px;
 width: 500px;
 border: 1px solid black;
-margin: 0 auto;
+margin: auto;
 `
 const ContainerSecundario = styled.div`
 padding: 15px;
@@ -56,8 +58,33 @@ class App extends React.Component {
 
     .then ((res) => {
       console.log(res.data)
-     
+      this.setState ({list: res.data })
+
     })
+
+    .catch ((err) => {
+     console.log (err)
+    })
+
+  }
+
+  deleteUsers = (id) => {
+
+
+    axios.delete (`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+     {
+
+      headers: {
+        Authorization: 'guilherme-amaral-lovelace'
+      }
+    })
+
+    .then ((res) => {
+      console.log(res)
+      alert ('Removido')
+      
+    })
+
     .catch ((err) => {
      console.log (err)
     })
@@ -84,9 +111,18 @@ class App extends React.Component {
 
   render () {
     
+    const listUsers = this.state.list.map ((user)=> { 
+      return <ContainerSecundario>
+        <p><hr />{user.name}</p>
+        <button onClick={() => this.deleteUsers(user.id)}>Remover</button>
+        </ContainerSecundario>
+    }
+    )
+    
+
     return (
       <ContainerPrincipal>
-        
+        <h1>Labusers</h1>
         <ContainerSecundario>
           <label>Nome:</label>
           <input 
@@ -106,9 +142,13 @@ class App extends React.Component {
         </ContainerSecundario>
 
         <div>
-          <button onClick={this.getAllUsers}>Ir para pagina de listas</button>
+          <button onClick={this.getAllUsers}>Lista de usuários</button>
         </div>
-
+        
+        <ContainerSecundario>
+          {listUsers}
+        </ContainerSecundario>
+        
       </ContainerPrincipal>
     )
   }
