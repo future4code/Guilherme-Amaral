6cefbcbc623864880c5aecb9509a12a15903a566
Guilherme Augusto de Almeida Amaral <85243403+GuilhermeAAmaral@users.noreply.github.com>
2021-11-24 21:49:31 -0300

    case-processo-seletivo

---
+++ semana23/case_promobit/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import GlobalState from './constantes/global/GlobalState'
+
+function App() {
+
+  return (
+
+    <GlobalState> 
+      <h1>Hello World</h1>
+    </GlobalState>
+  )
+}
+
+export default App;
